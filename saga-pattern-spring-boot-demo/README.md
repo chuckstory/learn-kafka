92b2d448d201ad5e0fa7837188f6b09f0bc443e3
Poowadon Meesilp <poowadon.ingeni@gmail.com>
2025-03-30 11:41:45 +0700

    remove

--- saga-pattern-spring-boot-demo/README.md
+++
@@ -1,3 +0,0 @@
-# saga-pattern-spring-boot-demo
-
-Demonstration of SAGA Orchestration Design Pattern using Spring Boot and Kafka
--- saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.appsdeveloperblog.orders.service;
-
-import com.appsdeveloperblog.core.dto.Order;
-
-public interface OrderService {
-    Order placeOrder(Order order);
-}
