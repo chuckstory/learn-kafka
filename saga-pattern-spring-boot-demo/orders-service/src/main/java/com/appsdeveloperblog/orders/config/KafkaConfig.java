5ce88b505d25194f1ef984dea39d9f699f16e5e6
chuckstory <poowadon.ingeni@gmail.com>
2025-04-01 22:07:29 +0700

    end of saga happy path

--- saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
+++ saga-pattern-spring-boot-demo/orders-service/src/main/java/com/appsdeveloperblog/orders/config/KafkaConfig.java
@@ -13,6 +13,17 @@ public class KafkaConfig {
 
     @Value("${orders.events.topic.name}")
     private String ordersEventsTopicName;
+
+
+    @Value("${products.commands.topic.name}")
+    private String productsCommandsTopicName;
+
+    @Value("${payments.commands.topic.name}")
+    private String paymentsCommandsTopicName;
+
+    @Value("${orders.commands.topic.name}")
+    private String ordersCommandsTopicName;
+
     private final static Integer TOPIC_REPLICATION_FACTOR = 3;
     private final static Integer TOPIC_PARTITIONS = 3;
 
@@ -29,4 +40,27 @@ public class KafkaConfig {
                 .build();
     }
 
+    @Bean
+    NewTopic createProductsCommandsTopic(){
+        return TopicBuilder.name(productsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createPaymentsCommandsTopic(){
+        return TopicBuilder.name(paymentsCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
+
+    @Bean
+    NewTopic createOrdersCommandsTopic(){
+        return TopicBuilder.name(ordersCommandsTopicName)
+                .partitions(TOPIC_PARTITIONS)
+                .replicas(TOPIC_REPLICATION_FACTOR)
+                .build();
+    }
 }
